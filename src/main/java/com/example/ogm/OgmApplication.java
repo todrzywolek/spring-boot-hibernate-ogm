5cd031ef0c09f7eed7d61e0b561f59f109f8a432
Tomasz Odrzywolek <tomeko92@gmail.com>
2019-12-31 23:34:45 +0100

    do small cleanup

--- src/main/java/com/example/ogm/OgmApplication.java
+++ src/main/java/com/example/ogm/OgmApplication.java
@@ -2,16 +2,12 @@ package com.example.ogm;
 
 import com.example.ogm.spring.Customer;
 import com.example.ogm.spring.CustomerRepository;
-import org.hibernate.ogm.jpa.HibernateOgmPersistence;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import javax.persistence.spi.PersistenceProvider;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableJpaRepositories
@@ -19,33 +15,6 @@ public class OgmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OgmApplication.class, args);
-    }
-
-//	@Bean
-//	public PlatformTransactionManager transactionManager() {
-//		return new JpaTransactionManager();
-//	}
-
-//	@Bean
-//	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//		LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-//		entityManager.setPersistenceUnitName("mongoPersistenceUnit");
-//		entityManager.setJpaVendorAdapter(new MyJpaVendorAdapter());
-//		Map<String, String> properties = new HashMap<>();
-//		properties.put("javax.persistence.transactionType", "resource_local");
-//		properties.put("hibernate.ogm.datastore.provider", "com.example.ogm.provider.XMongoDBDatastoreProvider");
-//		properties.put("hibernate.ogm.datastore.database", "test");
-//		entityManager.setJpaPropertyMap(properties);
-//		entityManager.setPersistenceProviderClass(HibernateOgmPersistence.class);
-//
-//		return entityManager;
-//	}
-
-    class MyJpaVendorAdapter extends HibernateJpaVendorAdapter {
-        @Override
-        public PersistenceProvider getPersistenceProvider() {
-            return new HibernateOgmPersistence();
-        }
     }
 
     @Bean
